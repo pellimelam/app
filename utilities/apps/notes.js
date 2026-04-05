3e010abb930491b3b204bf2088667409a2a344d7
pellimelam <pellimelam01@gmail.com>
2026-04-06 02:43:50 +0530

    Update notes.js

--- utilities/apps/notes.js
+++ utilities/apps/notes.js
@@ -895,23 +895,22 @@ window.downloadPage = async function(noteId, pageId){
 
 window.addEventListener("popstate", async ()=>{
 
+  // 🔒 isolate app
+  if(!location.pathname.includes("/notes")) return;
+
   const hash = window.location.hash;
 
-  // BACK TO NOTES
   if(!hash){
     loadNotesApp();
     return;
   }
 
-  // BACK TO PAGES (FIXED)
   if(hash === "#pages"){
-    const notes = await getNotes();
     if(__currentNoteId){
       openNote(__currentNoteId);
     }else{
       loadNotesApp();
     }
-    return;
   }
 
 });
